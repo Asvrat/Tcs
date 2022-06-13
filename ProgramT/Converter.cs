cbcb6cbe38e6c7bb527d28bbfd54708b5a1b142d
Asvrat <manulandx@gmail.com>
2022-06-13 21:47:40 +0300

    test commit

---
+++ ProgramT/Converter.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramOne
+{
+    public class Converter
+    /* 
+        1. Открыть .txt файл
+        2. Прочитать .txt файл 
+        3. Перевод и сохранения данных в массиве
+        4. Сохранение данных в новом текстовом документе
+    */
+    {
+        //private static string reader
+    }
+}
---
+++ ProgramT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramT
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "ProgramT\\example.txt";
+            if (File.Exists(path))
+            {
+                System.Console.WriteLine("yes");
+            }
+            else System.Console.WriteLine("no");
+        }
+    }
+}
